e8f9eb433eae7396866a9dfdd8ec22ecdad5df7d
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-08-06 18:15:40 +0430

    get all elements by id

---
+++ script.js
@@ -0,0 +1,10 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
